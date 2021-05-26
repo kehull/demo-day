aefd669644f1c9fb45db5d402347369f19e2bf4e
David Fournie <drfournie@gmail.com>
2021-05-26 16:37:20 -0500

    fixed api filter and added comments

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ def analysis():
 
 @app.route("/model", methods=["GET","POST"])
 def send():
+    #create empty list to transfer to html
     global table_data
     table_data=[]
     
@@ -59,14 +60,19 @@ def send():
     
     #postgres form setup and table vars
     if request.method == "POST":
+        
         #get info from forms
-        gender = request.form["gender"]
+        gender = request.form["gender"].upper() 
         income=request.form["income"]
         customer_id=uuid.uuid4().hex
         date=dt.datetime.today().strftime('%Y%m%d')
+        
+        #create and store sql session
         customer=Customer(customer_id=customer_id,gender=gender,income=income,membership_date=date)
         db.session.add(customer)
         db.session.commit() 
+        
+        #translate gender values to number for json dictionaries
         if gender== "M":
             gender=0
         elif gender=="O":
@@ -74,20 +80,29 @@ def send():
         else: 
             gender=2
         
-# Opening JSON file
+        # Opening JSON file
         f = open('Resources/models_kelly/encoding_keys/offers_encoded.json')
         g = gender
         i = int(income) 
   
-# returns JSON object as a dictionary
+        # load in knn model and json
         complete_knn= joblib.load('Resources/models_kelly/complete_offer.pkl')
         data = json.load(f)
+        
+        # returns JSON object as a dictionaries
         for dictionary in data:
             
+            #update dictionary with form values
             dictionary.update(gender = g, income = i,)
+            
+            #create variable for X values used in model
             model_data=[[dictionary['offer_id'],dictionary['gender'],dictionary['income'],dictionary['reward'],dictionary['channels'],dictionary['difficulty'],dictionary['duration'],dictionary['offer_type']]]
             complete_score = int(complete_knn.predict(model_data))
+            
+            #create key value for prediction
             dictionary['offer_completed_y_n']=complete_score
+            
+            #translate numbers into offer names
             if dictionary['offer_id'] ==8:
                 dictionary['offer_id']='Offer 7: The Americano'
             elif dictionary['offer_id']==2:
@@ -109,6 +124,7 @@ def send():
             else:
                 dictionary['offer_id']='Offer 1: The Doppio'
                              
+            #translate gender back into abbreviations
             if dictionary['gender'] ==0:
                 dictionary['gender']='M'
             elif dictionary['gender']==1:
@@ -116,6 +132,7 @@ def send():
             else:
                  dictionary['gender']='F'
             
+            #translate channels to respective strings
             if dictionary['channels'] ==1: 
                 dictionary["channels"]="email, mobile, social"
             elif dictionary['channels']==3: 
@@ -125,7 +142,7 @@ def send():
             else:
                 dictionary["channels"]="web, email"
             
-            
+            #translate offer types to respective strings
             if dictionary['offer_type'] ==1: 
                 dictionary['offer_type']="informational"
             elif dictionary['offer_type']==2: 
@@ -137,6 +154,8 @@ def send():
                 dictionary['offer_completed_y_n']="Yes"
             else:
                 dictionary['offer_completed_y_n']="No"
+            
+            #append dictionary to table data
             table_data.append(dictionary)
         f.close()
         
@@ -167,36 +186,51 @@ def showme():
 @app.route('/customer/api')
 def customer():
     #set up api
+    
     #query from db
     results = db.session.query(Customer.customer_id, Customer.gender,Customer.income,Customer.membership_date).all()
+    
     #set up list of dictonaries to store end result
     customer_data = {"customer_data":[]}
+    
     #loop through results and create dictionary
     for customer_id,gender,income,membership_date in results:
-
         test_data = {
-            
             "customer_id": customer_id,
             "gender": gender,
             "income": income,
             "membership_date": membership_date
         }
+        
         #append dictionary to customer_data
         customer_data["customer_data"].append(test_data)
     return jsonify(customer_data)
 @app.route('/customer/api/gender/<gender>')
 def gender(gender):
+    #create a list for checking if the gender variable is valid
     gender_list=['M','F','O']
+    
+    # convert any entry to uppercase
     canonicalization=gender.upper()
+    
+    #check if in gender list
     if canonicalization in gender_list:
+        
+        #set up list of dictonaries to store end result
         json_dict={"customer_data":[]}
+        
+        #query from db
         results=db.session.query(Customer.customer_id, Customer.gender,Customer.income,Customer.membership_date).filter(Customer.gender==canonicalization)
+         
+         #loop through results and create dictionary
         for customer_id,gender,income,membership_date in results:
             test_dict={}
             test_dict["customer_id"]=customer_id
             test_dict["gender"]=gender
             test_dict["income"]=income
             test_dict["membership_date"]=membership_date
+            
+            #append to json_dict
             json_dict["customer_data"].append(test_dict)
     else:
         return f'Error! Gender not found. Try "M","F", or "O".',404
@@ -205,12 +239,25 @@ def gender(gender):
 
 @app.route('/customer/api/income/<income>')
 def income(income):
+    # convert any entry to uppercase
     canonicalization=int(income)
+    
+    #query db for income values
     income_results=db.session.query(Customer.income).all()
+    
+    #store income values to list
     income_list=[i[0] for i in income_results]
+    
+    #check if form is valid
     if canonicalization >= min(income_list):
+        
+        #query db
         results= db.session.query(Customer.customer_id,Customer.gender,Customer.income,Customer.membership_date).filter(Customer.income <= canonicalization)
+        
+        #set up list of dictonaries to store end result
         json_dict={"customer_data":[]}    
+        
+        #loop through results and create dictionary
         for customer_id,gender,income,membership_date in results:
             test_dict={}
             test_dict["customer_id"]=customer_id
@@ -220,7 +267,7 @@ def income(income):
             json_dict["customer_data"].append(test_dict)
         return jsonify(json_dict)
     else:
-        return f"{income} is beyond the lower limit of the income range. Try a higher income.",404
+        return f"{income} is beyond the lower limit of the income range. Try entering a higher income value.",404
     
 
     
